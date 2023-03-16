f33d2b223dd654880072983ab03ee4c67838bbfe
yusup <yusupsharafutdinov@yandex.ru>
2023-03-16 11:55:34 +0300

    deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('Все работает как надо!')
 
 
 def second_page(request):
